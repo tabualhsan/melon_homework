a6cef045ccc5ee7311b4f8fdc3d5046d8179550c
Tamara Abualhsan <75860043+tabualhsan@users.noreply.github.com>
2021-01-12 08:14:04 -0800

    Add files via upload

---
+++ melon_info.py
@@ -0,0 +1,24 @@
+"""Print out all the melons in our inventory."""
+
+
+from melons import melons
+
+
+def print_all_melon(melons):
+    """Print each melon with corresponding attribute information."""
+
+    for key, value melons.items():
+        print(melon_name)
+    
+    for keys, values in keys.items():
+        print(f'{keys}: {values}')
+
+    # have_or_have_not = 'have'
+    # if seedless:
+    #     have_or_have_not = 'do not have'
+
+    # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+
+print_all_melon(melons)
